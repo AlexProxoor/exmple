46533d5b351a7c3a008924eebf5140d3796e21da
AlexProxoor <alex.prokhor03@gmail.com>
2024-06-26 17:23:50 +0300

    Add files via upload

---
+++ FavoritesContext.js
@@ -0,0 +1,29 @@
+import React, { createContext, useState, useContext } from 'react'
+
+const FavoritesContext = createContext()
+
+export const useFavorites = () => {
+  return useContext(FavoritesContext)
+}
+
+export const FavoritesProvider = ({ children }) => {
+  const [favorites, setFavorites] = useState([])
+
+  const addFavorite = (book) => {
+    setFavorites((prevFavorites) => [...prevFavorites, book])
+  }
+
+  const removeFavorite = (bookId) => {
+    setFavorites((prevFavorites) => prevFavorites.filter(book => book.id !== bookId))
+  }
+
+  const isFavorite = (bookId) => {
+    return favorites.some(book => book.id === bookId)
+  }
+
+  return (
+    <FavoritesContext.Provider value={{ favorites, addFavorite, removeFavorite, isFavorite }}>
+      {children}
+    </FavoritesContext.Provider>
+  )
+}
